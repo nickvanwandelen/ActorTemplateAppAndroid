615999a67aecec5aa746ecfe517b143a5011646e
Nick <nick.vanwandelen@hotmail.com>
2017-04-04 11:58:38 +0200

    added firebase showing projects

--- app/src/main/java/com/example/nick/actortemplateapp/MainActivity.java
+++ app/src/main/java/com/example/nick/actortemplateapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -12,7 +13,11 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +31,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
 
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
+
+    private ArrayList<Project> projects;
+
     public MainActivity(){
+        projects = new ArrayList<Project>();
+
+
+
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference();
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.e("Count", "Amount of projects: " + dataSnapshot.getChildrenCount());
+
+                projects.clear();
+
+                for(DataSnapshot postSnapshot: dataSnapshot.getChildren()){
+                    Project retrievedProject = postSnapshot.getValue(Project.class);
+                    projects.add(retrievedProject);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("Reading Firebase", databaseError.getMessage());
+            }
+        });
+
 
     }
 
@@ -37,10 +74,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
-        if(mFirebaseUser == null){
+        /*if(mFirebaseUser == null){
             startActivity(new Intent(this, LoginActivity.class));
             finish();
-        }
+        }*/
 
         ((Toolbar)findViewById(R.id.toolbar)).setTitle("Actor Template App");
 
@@ -48,12 +85,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void createProjectButtons(){
-        for(int i = 0; i < 11; i++){
+        for(Project project: projects){
             Button button = new Button(this);
             button.setOnClickListener(this);
-            button.setText("Button " + i);
+            button.setText(project.getName());
             button.setBackgroundColor(Color.CYAN);
-            button.setId(i);
 
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
             int margin = (int) (20 * this.getResources().getDisplayMetrics().density);
@@ -66,12 +102,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private List<Project> getSavedProjects(){
+    private void getSavedProjects(){
         List<Project> allProjects = new ArrayList<Project>();
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-        return allProjects;
+
+
+
     }
 
     @Override
